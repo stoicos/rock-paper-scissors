ad59e88bbb80daead0000f828f2fe174e9e39122
stoicos <marcelo.anavia.web@gmail.com>
2024-01-20 12:50:32 -0300

    Create a div that displays the best of five game

--- main.js
+++ main.js
@@ -13,8 +13,10 @@ function getComputerChoice() {
 let playerScore = 0;
 let computerScore = 0;
 let result = "Empieza una ronda"
+let game = false;
 
 function roundPlay(playerSelection) {
+
     let computerSelection = getComputerChoice();
 
     if (playerSelection === "rock" && computerSelection === "rock") {
@@ -42,6 +44,7 @@ function roundPlay(playerSelection) {
         result = 'Ganaste :D (Paper)';
         playerScore++;
     }
+    
 }
 
 let btnRock = document.createElement("button");
@@ -56,19 +59,50 @@ let body = document.querySelector("body");
 body.append(btnRock,btnPaper,btnScissors);
 
 let results = document.createElement("div");
+let score = document.createElement("div");
 
 btnRock.addEventListener('click', () => {
     roundPlay('rock');
+    if(playerScore >= 5 || computerScore >= 5) {
+        game = true
+        gameIsOver()
+        return
+    }   
     results.textContent = result;
-
+    score.textContent = `Tu puntaje: ${playerScore} \n El puntaje de la máquina: ${computerScore}`
+    gameIsOver()
 })
 btnPaper.addEventListener('click', () => {
     roundPlay('paper');
+    if(playerScore >= 5 || computerScore >= 5) {
+        game = true
+        gameIsOver()
+        return
+    }
     results.textContent = result;
+    score.textContent = `Tu puntaje: ${playerScore} \n El puntaje de la máquina: ${computerScore}`
+    gameIsOver()
 })
 btnScissors.addEventListener('click', () => {
     roundPlay('scissors');
+    if(playerScore >= 5 || computerScore >= 5) {
+        game = true
+        gameIsOver()
+        return
+    }
     results.textContent = result;
+    score.textContent = `Tu puntaje: ${playerScore} \n El puntaje de la máquina: ${computerScore}`
+
 })
 
-body.append(results);
+function gameIsOver() {
+    if(game === true && playerScore > computerScore) {
+        score.textContent = `Ganaste ${playerScore} a ${computerScore}`
+    } else if (game === true && playerScore < computerScore) {
+        score.textContent = `Perdiste ${computerScore} a ${playerScore}`
+    } else if (game === true && playerScore === computerScore) {
+        score.textContent = "Empate"
+    }
+}
+
+body.append(results, score);
